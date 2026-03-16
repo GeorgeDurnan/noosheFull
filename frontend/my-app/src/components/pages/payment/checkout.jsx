9adf184a52de8388e881be6497e0e3502b1fa15e
GeorgeDurnan <gfjdurnan@gmail.com>
2026-03-16 10:09:55 +0000

    made it so the you would scroll back to the top when viewing the checkout as the link was at the bottom

--- frontend/my-app/src/components/pages/payment/checkout.jsx
+++ frontend/my-app/src/components/pages/payment/checkout.jsx
@@ -4,10 +4,13 @@ import { useSelector } from "react-redux"
 import { getFullAddressFromSlice } from "../../../features/slices/addressSlice"
 import { AddressCheckout } from "./addressCheckout"
 import styles from "./addressCheckout.module.css"
+import { useEffect } from "react"
 export const Checkout = () => {
     const cart = useCreateCartItems()
     const fullAddress = useSelector(getFullAddressFromSlice)
-
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
     if (cart === "loading") {
         return (
             <h1>No items</h1>
@@ -17,7 +20,7 @@ export const Checkout = () => {
     } else {
         return (
             <div className={styles.checkout}>
-                <StripeEmbed  cart={cart.arranged} address_id={fullAddress.id} />
+                <StripeEmbed cart={cart.arranged} address_id={fullAddress.id} />
             </div>
         )
     }
